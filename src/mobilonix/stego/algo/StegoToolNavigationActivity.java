8c73e29cffa0cee8496c01584572af6c6048a523
Shafik.Quoraishee <Shafik.Quoraishee@viacommix.com>
2013-04-27 20:16:48 -0400

    FUCK YOU

--- src/mobilonix/stego/algo/StegoToolNavigationActivity.java
+++ src/mobilonix/stego/algo/StegoToolNavigationActivity.java
@@ -1,7 +1,35 @@
 package mobilonix.stego.algo;
 
 import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
 
 public class StegoToolNavigationActivity extends TabActivity {
 
+	Resources res;
+	TabHost tabHost;
+	Intent intent;
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stego_tool_nav_activity);
+
+        res = getResources();
+        tabHost = getTabHost();
+        TabHost.TabSpec spec;
+
+
+        intent = new Intent().setClass(this, StegoEncodeActivity.class);
+        spec = tabHost.newTabSpec("calendar").setIndicator("Calendar", res.getDrawable(R.drawable.ic_action_search)).setContent(intent);
+        tabHost.addTab(spec);
+
+        intent = new Intent().setClass(this, StegoToolDecodeActivity.class);
+        spec = tabHost.newTabSpec("profile").setIndicator("Profile", res.getDrawable(R.drawable.ic_action_search)).setContent(intent);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(0);
+ }
+	
 }
--- src/mobilonix/stego/algo/StegoToolWelcomeActivity.java
+++ src/mobilonix/stego/algo/StegoToolWelcomeActivity.java
@@ -1,9 +1,19 @@
 package mobilonix.stego.algo;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
 
 public class StegoToolWelcomeActivity extends Activity {
-
 	
+	Button nextButton;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		setContentView(R.layout.stego_tool_welcome_activity);
+		
+		
+	}
 	
 }
